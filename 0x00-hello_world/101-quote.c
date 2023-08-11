442fdcba72bb5b89f9e1fbc8e30db8039e95289f
macahmed1 <malikprog>
2023-08-10 22:20:18 -0700

    C program that prints exact statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
  * main - program entry point
- * Return: program returns 1
+ *
+ * Return: program return 0
+ *
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
