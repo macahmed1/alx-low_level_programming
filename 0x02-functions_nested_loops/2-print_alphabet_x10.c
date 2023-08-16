268c95c473abc711c8b6e4e537954e89b8d37b8f
macahmed1 <malikprog>
2023-08-15 22:03:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Printing alphabet in lowercase up to 10 times
+ * print_alphabet_x10 - Printing alphabet in lowercase 10 times
  * Return: Always 0
  */
 void print_alphabet_x10(void)
