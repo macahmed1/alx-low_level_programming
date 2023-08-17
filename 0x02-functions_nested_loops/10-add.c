0fcc456f07649851ec043bb0254bfea2b2968327
macahmed1 <malikprog>
2023-08-16 22:25:38 -0700

    Function to add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Function toadd two integers
+ * @x: int type x number
+ * @y: int type y number
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
