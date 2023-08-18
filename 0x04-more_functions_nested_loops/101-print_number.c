fe8e31675b2bdf4653a27ac7e79f7c17f9fdc105
macahmed1 <malikprog>
2023-08-17 23:43:31 -0700

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,63 +7,46 @@
  * Description: Can only use _putchar to print.
  * Return: Nothing!
  */
+
 void print_number(int n)
 {
+	long m; /* power of 10 */
+	int c; /* boolean check */
+	long num; /* convert int to long */
 
-/*
-//	long m; /* power of 10 */
-//	int c; /* boolean check */
-//	long num; /* convert int to long */
-
-//	num = n;
-//	/* negatives */
-//	if (num < 0)
-//	{
-//		num *= -1;
-//		_putchar('-');
-//	}
-
-//	/* count up */
-//	m = 1;
-//	c = 1;
-//	while (c)
-//	{
-//		if (num / (m * 10) > 0)
-//			m *= 10;
-//		else
-//			c = 0;
-//	}
-
-//	/* count down */
-//	while (num >= 0)
-//	{
-//		if (m == 1)
-//		{
-//			_putchar(num % 10 + '0');
-//			num = -1;
-//		}
-//		else
-//		{
-//			_putchar((num / m % 10) + '0');
-//			m /= 10;
-//		}
-//	}
-
-
-//	CODEGEN
-
-	unsigned int k = n;
-
-	if (n<0)
+	num = n;
+	/* negatives */
+	if (num < 0)
 	{
-	m *= -1;
-	k = n;
-	_putchar('-');
+		num *= -1;
+		_putchar('-');
 	}
-	k /=10;
-	if (k != 0)
-	print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
+
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
 
 
 }
