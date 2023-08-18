691c76b2a560f656f93b2a9495e6e426c2da2d8a
macahmed1 <malikprog>
2023-08-17 17:12:13 -0700

    Debugging main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checking if function prints positive or negative integer.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
