1f577ed83176aa143160a2485f01c08e9801e853
macahmed1 <malikprog>
2023-08-30 21:18:54 -0700

    Function that returns 1 if the input integer is a prime number, otherwise return 0.

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Function that returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: Value number of int.
+ * Return: This returns  1 if is prime. Else, returns 0 if it is otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n < 4)
+		return (1);
+	return (hai(n, 2));
+}
+
+/**
+ * _sqrt - This is returns the  square root of number
+ * @x: number value x
+ * @i: number i incrementer.
+ * Return: This returns the square root of `x`
+ */
+
+int _sqrt(int x, int i)
+{
+	int square;
+
+	square = i * i;
+	if (square >= x)
+		return (i);
+	else
+		return (_sqrt(x, i + 1));
+}
+
+/**
+ * hai - The function for help.
+ * @n: The original function give is is_prime_number
+ * @d: incrementer entry
+ * Return:  Returns 1 if is prime. Else returns 0 if not prime.
+ */
+
+int hai(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+	else if (_sqrt(n, 1) < d)
+		return (1);
+	else
+		return (hai(n, d + 1));
+}
