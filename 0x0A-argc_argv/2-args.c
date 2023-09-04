d70ad94ca5441c0a80b3fe0eeea5755f01fbef03
macahmed1 <malikprog>
2023-09-04 01:06:42 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,8 +9,11 @@
  */
 
 int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
