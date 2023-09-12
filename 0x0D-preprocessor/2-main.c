cc9db1e4787a6bd3e2fa48ca4ccaf1b10daaa312
macahmed1 <malikprog>
2023-09-11 19:15:31 -0700

    Program to print the name a file is printed from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print the name a file is compiled from.
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
