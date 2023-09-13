305b0a8af9b4578a72aa5743dd67f22f5245d9f0
macahmed1 <malikprog>
2023-09-13 12:43:11 -0700

    A program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: The operator passed as an argument
+ * Return: A pointer to the corresponding function, or NULL if not found
+ */
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+
+    int i = 0;
+
+    while (ops[i].op)
+    {
+        if (*(ops[i].op) == *s)
+            return (ops[i].f);
+        i++;
+    }
+
+    return (NULL);
+}
+
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: This is the argument count
+ * @argv: The string of arguments vector in array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*operation)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+
+    operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
