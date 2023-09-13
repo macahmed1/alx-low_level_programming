88137decc2352cb353deb02b1d54b4025433a91d
macahmed1 <malikprog>
2023-09-13 12:39:43 -0700

    A program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main entry
+ * @print_opcodes - Printing op codes
+ * @num_bytes: Bytes number
+ */
+
 void print_opcodes(int num_bytes);
 
-int main(int argc, char *argv[]) {
-    int num_bytes;
+int main(int argc, char *argv[])
+{
+	int num_bytes;
 
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    num_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
+	if (num_bytes < 0)
+	{
+	printf("Error\n");
+	return (2);
+	}
 
-    print_opcodes(num_bytes);
+	print_opcodes(num_bytes);
 
-    return 0;
+	return (0);
 }
 
-void print_opcodes(int num_bytes) {
-    char *ptr = (char *)print_opcodes;
-    int i;
+/**
+ * print_opcodes - Opcodes print.
+ * @num_bytes: Bytes number.
+ */
 
-    for (i = 0; i < num_bytes; i++) {
-        printf("%02x", ptr[i] & 0xFF);
-    }
+void print_opcodes(int num_bytes)
+{
+	char *ptr = (char *)print_opcodes;
+	int i;
 
-    printf("\n");
+	for (i = 0; i < num_bytes; i++)
+	{
+	printf("%02x", ptr[i] & 0xFF);
+	}
+
+	printf("\n");
 }
-
