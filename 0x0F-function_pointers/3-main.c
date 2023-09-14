872f49b5c38ea8ab93dc6b5379b080b56be4bae5
macahmed1 <malikprog>
2023-09-14 12:28:27 -0700

    A program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,20 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1, num2, result;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    operation = get_op_func(argv[2]);
-
+	operation = get_op_func(argv[2]);
+	
 	if (operation == NULL)
 	{
 		printf("Error\n");
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,8 @@
  * @b: The second integer
  * Return: The sum of a and b
  */
-	int op_add(int a, int b)
+
+int op_add(int a, int b)
 {
 	return (a + b);
 }
